2adaebab2be09683b65d73fd0b19c12bf502fb30
underscorelior <newlioryt@gmail.com>
2024-07-19 14:09:38 -0700

    Create animated border button

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,6 +10,17 @@ export default function TestPage() {
 				options={['Quebec', 'United States', 'Mexico', 'Ottawa']}
 				answer={3}
 			/>
+			<button className='duration-400 ease hover:animate-button-scale group relative h-10 min-w-[20%] cursor-pointer overflow-hidden rounded-md px-4 py-2 transition-all'>
+				<div className='absolute left-0 top-0 h-full w-full rounded-md border-2 px-4 py-2'>
+					cuba
+				</div>
+				<div className='pointer-events-none absolute inset-0'>
+					<div className='before:group-hover:animate-button-width before:absolute before:left-0 before:top-0 before:h-[2px] before:w-0 before:rounded-tl-md before:rounded-tr-md before:bg-green-500'></div>
+					<div className='before:group-hover:animate-button-height before:absolute before:right-0 before:top-0 before:h-0 before:w-[2px] before:rounded-br-md before:rounded-tr-md before:bg-green-500'></div>
+					<div className='before:group-hover:animate-button-width2 before:absolute before:bottom-0 before:right-0 before:h-[2px] before:w-0 before:rounded-bl-md before:rounded-br-md before:bg-green-500'></div>
+					<div className='before:group-hover:animate-button-height2 before:absolute before:bottom-0 before:left-0 before:h-0 before:w-[2px] before:rounded-bl-md before:rounded-tl-md before:bg-green-500'></div>
+				</div>
+			</button>
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,11 +17,31 @@ const config: Config = {
 			backgroundSize: {
 				'200%': '200%',
 			},
-			animation: { answered: 'answered 0.5s cubic-bezier(1,0,.9,1)' },
+			animation: {
+				answered: 'answered 0.5s cubic-bezier(1,0,.9,1)',
+				'button-width': 'width 300ms ease 100ms forwards',
+				'button-height': 'height 200ms ease 300ms forwards',
+				'button-width2': 'width 100ms ease 500ms forwards',
+				'button-height2': 'height 0ms ease 600ms forwards',
+				'button-scale': 'scale 300ms ease-in-out 650ms forwards',
+			},
 			keyframes: {
 				answered: {
 					from: { 'background-position': '100%' },
 					to: { 'background-position': '0%' },
+				},
+				width: {
+					'0%': { width: '0' },
+					'100%': { width: '100%' },
+				},
+				height: {
+					'0%': { height: '0' },
+					'100%': { height: '100%' },
+				},
+				scale: {
+					'0%': { transform: 'scale(1)' },
+					'50%': { transform: 'scale(1.05)' },
+					'100%': { transform: 'scale(1)' },
 				},
 			},
 		},
