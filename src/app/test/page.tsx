12f11baeada7ab12084945934c60ddf1dc645534
underscorelior <newlioryt@gmail.com>
2024-07-19 17:59:30 -0700

    Start randomization

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,64 @@
+'use client';
+
 import MultipleChoiceQuiz from '@/components/quiz/multiple-choice';
+import worldJSON from '@/assets/countries.json';
+import React, { useEffect, useState } from 'react';
+
+type Item = {
+	flags: string;
+	name: string;
+	capital: string | string[];
+};
+
+type Quiz = {
+	question: React.ReactNode;
+	options: string[];
+	answer: string; // TODO: Make answer an item for easier stuff later on
+};
 
 export default function TestPage() {
+	const [quiz, setQuiz] = useState<Quiz | {}>({});
+
+	function generateNextQuestion() {
+		function arrStrToStr(str: string | string[]): string {
+			// Convert string or string array to string
+			return Array.isArray(str) ? str.join(', ') : str;
+		}
+
+		const countries: Item[] = worldJSON;
+
+		const options: Item[] = [];
+
+		const answerOption: Item =
+			countries[Math.floor(Math.random() * countries.length)];
+
+		options.push(answerOption);
+		countries.filter((item) => item !== answerOption);
+
+		for (let i = 0; i < 3; i++) {
+			const opt = countries[Math.floor(Math.random() * countries.length)];
+			options.push(opt);
+			countries.filter((item) => item !== opt);
+		}
+
+		for (let i = options.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * i);
+			[options[i], options[j]] = [options[j], options[i]];
+		}
+
+		const output: Quiz = {
+			question: 'Test',
+			options: options.map((option) => arrStrToStr(option.capital)),
+			answer: arrStrToStr(answerOption.capital),
+		};
+
+		console.log(output);
+	}
+
+	useEffect(() => {
+		generateNextQuestion();
+	}, []);
+
 	return (
 		<div className='p-10'>
 			<MultipleChoiceQuiz
