f9d288cae5fff06ad741448c891bf79b8fa9b3ed
Lior Pendler <lior.pendler@gmail.com>
2024-07-22 14:44:18 -0700

    Start with auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import AuthButton from '@/components/auth/auth-button';
 import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-center gap-y-6 p-24 text-2xl font-semibold'>
+			<AuthButton />
 			<Link href={'/capitals/us'}>US Capitals</Link>
 			<Link href={'/flags/us'}>US Flags</Link>
 			<Link href={'/capitals/world'}>World Capitals</Link>
---
+++ src/components/auth/auth-button.tsx
@@ -0,0 +1,37 @@
+import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default async function AuthButton() {
+	const supabase = createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	const signOut = async () => {
+		'use server';
+
+		const supabase = createClient();
+		await supabase.auth.signOut();
+		return redirect('/login');
+	};
+
+	return user ? (
+		<div className='flex items-center gap-4'>
+			Hey, {user.email}!
+			<form action={signOut}>
+				<button className='bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline'>
+					Logout
+				</button>
+			</form>
+		</div>
+	) : (
+		<Link
+			href='/login'
+			className='bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline'
+		>
+			Login
+		</Link>
+	);
+}
