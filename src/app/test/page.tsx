520e08a01e42fee57eb8dcd8e0e6aca95ea08fe5
Lior Pendler <lior.pendler@gmail.com>
2024-11-05 00:46:37 -0800

    Attempt to fix compile issues

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,7 +5,7 @@ import FlagQuiz from '@/components/quiz/mcq-flag';
 export default function TestPage() {
 	return (
 		<div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-y-6'>
-			<FlagQuiz options={worldJSON} />
+			<FlagQuiz options={worldJSON as Option[]} />
 			<div className='flex w-full max-w-[70%] justify-between text-lg font-semibold text-zinc-900 md:max-w-[40%]'>
 				{/* <h1 className='flex items-center justify-between'>
 				Capital Score: {capitalScore}
