dd5ce6be1a13b5d1b5119d035df60a3d1bb44568
Lior Pendler <lior.pendler@gmail.com>
2025-01-08 23:09:17 -0800

    Fix Vercel deployment

--- src/app/map/europe/page.tsx
+++ src/app/map/europe/page.tsx
@@ -6,7 +6,11 @@ import Link from 'next/link';
 export default function Map() {
 	return (
 		<div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-y-6'>
-			<MapQuiz options={euJSON} map={EUMapSVG} identifier='id' />
+			<MapQuiz
+				options={euJSON as unknown as Option[]}
+				map={EUMapSVG}
+				identifier='id'
+			/>
 			<div className='flex justify-between text-lg font-semibold text-zinc-900'>
 				<Link href='/' className='rounded-md border-2 px-4 py-3 text-center'>
 					Back to quizzes
--- src/app/map/us/page.tsx
+++ src/app/map/us/page.tsx
@@ -6,7 +6,11 @@ import USMapSVG from '@/assets/maps/us';
 export default function Map() {
 	return (
 		<div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-y-6'>
-			<MapQuiz options={usJSON} map={USMapSVG} identifier='class' />
+			<MapQuiz
+				options={usJSON as unknown as Option[]}
+				map={USMapSVG}
+				identifier='class'
+			/>
 			<div className='flex justify-between text-lg font-semibold text-zinc-900'>
 				<Link href='/' className='rounded-md border-2 px-4 py-3 text-center'>
 					Back to quizzes
