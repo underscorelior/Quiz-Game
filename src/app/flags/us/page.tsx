2081bf4a87b0dbfed09d5cfcbcf6a2d291a164f7
Lior Pendler <lior.pendler@gmail.com>
2025-01-08 23:17:37 -0800

    Typescript causing vercel deployment errors

--- src/app/flags/us/page.tsx
+++ src/app/flags/us/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Quiz() {
 	return (
 		<div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-y-6'>
-			<FlagQuiz options={usJSON} />
+			<FlagQuiz options={usJSON as never as Option[]} />
 			<div className='flex w-[90%] max-w-xl justify-between text-lg font-semibold text-zinc-900'>
 				<Link href='/' className='rounded-md border-2 px-4 py-3 text-center'>
 					Back to quizzes
